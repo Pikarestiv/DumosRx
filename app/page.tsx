3395472acc40cb43a0eb3e1707b164c81ba6a27e
pikarestiv <okwyodumodu@gmail.com>
2025-08-16 17:01:49 +0100

    chore: sync files

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { BusinessIntelligenceDashboard } from "../client/components/analytics/business-intelligence-dashboard"
-
-export default function SyntheticV0PageForDeployment() {
-  return <BusinessIntelligenceDashboard />
-}

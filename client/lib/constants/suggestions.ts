5bd4ff8a7fda552a331a3572f4b8c8113fccc51c
pikarestiv <okwyodumodu@gmail.com>
2026-05-15 00:07:24 +0100

    feat: make form suggestions exhaustive with 150+ Nigerian pharma and retail entries

--- client/lib/constants/suggestions.ts
+++ client/lib/constants/suggestions.ts
@@ -4,32 +4,53 @@ export const FORM_SUGGESTIONS = {
       "Paracetamol", "Panadol", "Emzor Paracetamol", "Amoxicillin", "Augmentin", 
       "Ciprofloxacin", "Ciprotab", "Metformin", "Glucophage", "Omeprazole", 
       "Losec", "Artemether/Lumefantrine", "Coartem", "Lonart", "Amatem", 
-      "Vitamin C", "Cevit", "B-Complex", "Diclofenac", "Voltaren", 
-      "Ibuprofen", "Advil", "Nurofen", "Albendazole", "Zentel"
+      "Lonart DS", "Coartem 80/480", "Vitamin C", "Cevit", "B-Complex", 
+      "Diclofenac", "Voltaren", "Ibuprofen", "Advil", "Nurofen", "Albendazole", 
+      "Zentel", "Gaviscon", "Gestid", "Andrews Liver Salt", "Milk of Magnesia",
+      "Rocephin", "Ceftriaxone", "Azithromycin", "Zithromax", "Cefuroxime", "Zinnat",
+      "Amlodipine", "Norvasc", "Lisinopril", "Zestril", "Losartan", "Cozaar",
+      "Metronidazole", "Flagyl", "Artesunate", "P-Alaxin", "Lofem", "Postinor-2",
+      "Durex", "Rough Rider", "Oral Rehydration Salt (ORS)", "Ventolin", "Salbutamol",
+      "Insulin", "Humulin", "Atorvastatin", "Lipitor", "Hydrochlorothiazide",
+      "Prednisolone", "Dexamethasone", "Fersolate", "Blood Tonic", "Astymin",
+      "Seven Seas", "Cod Liver Oil", "Mist Mag", "Nystatin", "Clotrimazole", "Canesten"
     ],
     generics: [
       "Acetaminophen", "Amoxicillin/Clavulanate", "Ciprofloxacin", "Metformin Hydrochloride", 
       "Omeprazole", "Artemether/Lumefantrine", "Ascorbic Acid", "Diclofenac Sodium", 
       "Ibuprofen", "Albendazole", "Cetirizine", "Loratadine", "Amlodipine", 
-      "Lisinopril", "Metronidazole", "Azithromycin"
+      "Lisinopril", "Metronidazole", "Azithromycin", "Cefuroxime", "Ceftriaxone",
+      "Artesunate", "Atorvastatin", "Hydrochlorothiazide", "Prednisolone", 
+      "Dexamethasone", "Salbutamol", "Furosemide", "Glibenclamide", "Atenolol",
+      "Losartan", "Simvastatin", "Captopril", "Nifedipine", "Spironolactone",
+      "Clotrimazole", "Nystatin", "Ketoconazole", "Fluconazole", "Griseofulvin",
+      "Doxycycline", "Tetracycline", "Erythromycin", "Clarithromycin", "Levofloxacin"
     ],
     strengths: [
       "500mg", "1g", "250mg", "125mg", "100mg", "50mg", "20mg", "10mg", "5mg", 
-      "100ml", "200ml", "60ml", "15ml", "5ml", "IU", "mcg"
+      "2.5mg", "40mg", "80mg", "160mg", "320mg", "625mg", "1.2g",
+      "100ml", "200ml", "60ml", "15ml", "5ml", "10ml", "50ml",
+      "IU", "mcg", "µg", "units", "w/v", "w/w", "v/v", "percentage (%)"
     ],
     categories: [
       "Analgesics", "Antibiotics", "Antimalarials", "Vitamins & Supplements", 
       "Antacids", "Antihypertensives", "Antidiabetics", "Antifungals", 
-      "Antivirals", "Cough & Cold", "Dermatologicals", "Eye/Ear Drops"
+      "Antivirals", "Cough & Cold", "Dermatologicals", "Eye/Ear Drops",
+      "Anthelmintics", "Anticonvulsants", "Antidepressants", "Antihistamines",
+      "Family Planning", "First Aid", "Surgicals", "Medical Devices"
     ],
     dosageForms: [
       "Tablet", "Capsule", "Syrup", "Suspension", "Injection", "Infusion", 
-      "Cream", "Ointment", "Gel", "Drops", "Inhaler", "Suppository"
+      "Cream", "Ointment", "Gel", "Drops", "Inhaler", "Suppository",
+      "Pessary", "Powder", "Sachet", "Lotion", "Spray", "Patch"
     ],
     manufacturers: [
       "Emzor Pharmaceuticals", "Fidson Healthcare", "May & Baker Nigeria", 
       "GSK Nigeria", "Pfizer Nigeria", "Sanofi Nigeria", "M&B", "Juhel Nigeria", 
-      "Swiss Pharma", "Dana Pharmaceuticals"
+      "Swiss Pharma (Swipha)", "Dana Pharmaceuticals", "MeCure Industries",
+      "Neimeth International", "Geneith Pharmaceuticals", "Sam Pharmaceutical",
+      "Drugfield Pharmaceuticals", "Chi Pharmaceuticals", "Hovid", "Novartis",
+      "AstraZeneca", "Roche", "Sun Pharma (Ranbaxy)", "Bayer"
     ]
   },
   retail: {
@@ -37,33 +58,52 @@ export const FORM_SUGGESTIONS = {
       "Coca-Cola", "Pepsi", "Indomie Noodles", "Peak Milk", "Milo", "Bournvita", 
       "Dano Milk", "Kellogg's Corn Flakes", "Golden Morn", "Maggi", "Knorr", 
       "Ariel Detergent", "Sunlight", "Dettol Soap", "Lux Soap", "Close Up", 
-      "Oral B", "Hypo Bleach"
+      "Oral B", "Hypo Bleach", "Mamador Oil", "Devon King's Oil", "Checkers Custard",
+      "Macleans", "Colgate", "Savlon", "Pears Baby Oil", "Johnson's Baby",
+      "Harpic", "Jig", "Air Wick", "Always Sanitary Pad", "Molfix Diapers",
+      "Pampers", "Cway Water", "Eva Water", "Bigi Drinks", "Viju Milk"
     ],
     generics: [],
-    strengths: [],
+    strengths: [
+      "Small", "Medium", "Large", "Economy", "Family Pack", "Sachet",
+      "500g", "1kg", "2kg", "5kg", "10kg", "25kg", "50kg",
+      "35cl", "50cl", "60cl", "75cl", "1L", "2L", "5L", "20L",
+      "70g", "120g", "150g", "200g", "400g", "450g", "900g"
+    ],
     categories: [
       "Groceries", "Beverages", "Personal Care", "Household", "Snacks", 
-      "Dairy", "Baby Products", "Toiletries", "Food & Spices"
+      "Dairy", "Baby Products", "Toiletries", "Food & Spices", "Grains & Pasta",
+      "Cereals", "Cleaning Agents", "Laundry", "Kitchenware", "Stationery"
     ],
     dosageForms: [],
     manufacturers: [
       "Unilever Nigeria", "Nestle Nigeria", "PZ Cussons", "Dangote Group", 
-      "Flour Mills of Nigeria", "Promasidor", "Chi Limited", "Dufil Prima Foods"
+      "Flour Mills of Nigeria", "Promasidor", "Chi Limited", "Dufil Prima Foods",
+      "Tolaram Group", "Coca-Cola Hellenic", "Nigerian Breweries", "Guinness Nigeria",
+      "Cadbury Nigeria", "Honeywell Flour Mills", "BUA Group"
     ]
   },
   common: {
     units: [
       "Sachet", "Tablet", "Capsule", "Piece", "Bottle", "Pack", "Carton", 
-      "Box", "Roll", "Bag", "Jar", "Tube"
+      "Box", "Roll", "Bag", "Jar", "Tube", "Can", "Drum", "Crate", "Tray"
     ],
     locations: [
       "Lagos", "Abuja", "Port Harcourt", "Kano", "Ibadan", "Kaduna", "Benin City", 
-      "Enugu", "Onitsha", "Warri", "Jos", "Ilorin", "Abeokuta", "Uyo", "Calabar"
+      "Enugu", "Onitsha", "Warri", "Jos", "Ilorin", "Abeokuta", "Uyo", "Calabar",
+      "Owerri", "Akure", "Aba", "Bauchi", "Sokoto", "Maiduguri", "Zaria", "Minna",
+      "Makurdi", "Lokoja", "Asaba", "Awka", "Yola", "Jalingo", "Gombe", "Damaturu",
+      "Birnin Kebbi", "Gusau", "Dutse", "Katsina", "Lafia", "Abakaliki", "Osogbo", "Ado-Ekiti"
     ],
     states: [
-      "Lagos State", "FCT", "Rivers State", "Kano State", "Oyo State", "Kaduna State", 
-      "Edo State", "Enugu State", "Anambra State", "Delta State", "Plateau State", 
-      "Kwara State", "Ogun State", "Akwa Ibom State", "Cross River State"
+      "Abia State", "Adamawa State", "Akwa Ibom State", "Anambra State", "Bauchi State",
+      "Bayelsa State", "Benue State", "Borno State", "Cross River State", "Delta State",
+      "Ebonyi State", "Edo State", "Ekiti State", "Enugu State", "FCT - Abuja",
+      "Gombe State", "Imo State", "Jigawa State", "Kaduna State", "Kano State",
+      "Katsina State", "Kebbi State", "Kogi State", "Kwara State", "Lagos State",
+      "Nasarawa State", "Niger State", "Ogun State", "Ondo State", "Osun State",
+      "Oyo State", "Plateau State", "Rivers State", "Sokoto State", "Taraba State",
+      "Yobe State", "Zamfara State"
     ]
   }
 };

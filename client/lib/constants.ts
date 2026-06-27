0494488dd17856475b378b351e4765fabfcc1c2a
pikarestiv <okwyodumodu@gmail.com>
2026-06-27 18:28:38 +0100

    chore: bump version to v0.0.19

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "DumosRx";
-export const APP_VERSION = "0.0.18"; // DumosRx current version (update when bumping version)
+export const APP_VERSION = "0.0.19"; // DumosRx current version (update when bumping version)
 
 export const WEB_APP_URL = "https://dumosrx.com";
 export const API_BASE_URL = "https://api.dumosrx.com/api/v1";

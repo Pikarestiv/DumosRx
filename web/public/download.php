a8dcb42fc4aa48c22aaf272f9515cf588a1ae123
pikarestiv <okwyodumodu@gmail.com>
2026-07-10 12:37:38 +0100

    chore: bump remaining files to v0.0.22

--- web/public/download.php
+++ web/public/download.php
@@ -7,7 +7,7 @@
 $os = isset($_GET['os']) ? $_GET['os'] : 'windows';
 
 // Default version if updater.json is missing or fails
-$version = "0.0.21"; 
+$version = "0.0.22"; 
 $downloadUrl = "";
 
 // Fetch latest release data from DumosRx Server

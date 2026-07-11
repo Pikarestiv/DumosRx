1aa45664f61b9431a30e9842420f04a1ee9e0bbb
pikarestiv <okwyodumodu@gmail.com>
2026-07-11 12:00:00 +0100

    fix(tauri): resolve android app crash by adding explicit rustls crypto provider initialization

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,5 +1,20 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+  std::panic::set_hook(Box::new(|info| {
+      log::error!("CRASH_PANIC: {:?}", info);
+  }));
+
+  #[cfg(any(target_os = "android", target_os = "ios"))]
+  {
+      if rustls::crypto::CryptoProvider::get_default().is_none() {
+          if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+              log::error!("Failed to install rustls crypto provider: {:?}", e);
+          } else {
+              log::info!("Successfully installed rustls crypto provider");
+          }
+      }
+  }
+
   tauri::Builder::default()
     .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_sql::Builder::default().build())

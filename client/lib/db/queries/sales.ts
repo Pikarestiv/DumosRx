61e45977de80072a70eb4a513044fc05580bed39
pikarestiv <okwyodumodu@gmail.com>
2026-07-18 23:46:06 +0100

    fix: use sales item cost price instead of medicine cost price in sales query

--- client/lib/db/queries/sales.ts
+++ client/lib/db/queries/sales.ts
@@ -12,7 +12,7 @@ export async function getTransactionDetails(saleId: string) {
     `SELECT 
       si.*, 
       m.name as product_name, 
-      m.cost_price as med_cost_price,
+      si.cost_price as med_cost_price,
       COALESCE((
         SELECT SUM(ri.quantity) 
         FROM return_items ri 
